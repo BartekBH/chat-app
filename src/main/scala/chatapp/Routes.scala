68be443ea45b6a1cabc37e453526eae7ee16e9f9
Bartek <bartek@pop-os.home>
2025-02-13 15:47:14 +0100

    change rout

--- src/main/scala/chatapp/Routes.scala
+++ src/main/scala/chatapp/Routes.scala
@@ -29,7 +29,7 @@ class Routes[F[_]: Files: Temporal] extends Http4sDsl[F] {
     cs: Ref[F, ChatState]
   ): HttpApp[F] = {
     HttpRoutes.of[F] {
-      case request @ get -> Root / "chat.html" =>
+      case request @ get -> Root =>
         StaticFile
           .fromPath(
             fs2.io.file.Path(getClass.getClassLoader.getResource("chat.html").getFile),
